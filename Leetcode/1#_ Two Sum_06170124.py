ebc87a711d3815e52a8517e8cd305cae6741df9b
only980531 <42889639+only980531@users.noreply.github.com>
2020-01-10 02:36:34 +0800

    Add files via upload

---
+++ Leetcode/1#_ Two Sum_06170124.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            a = target-nums[i]
+            if a in nums and i!=nums.index(a):
+                return([i,nums.index(a)])
+
